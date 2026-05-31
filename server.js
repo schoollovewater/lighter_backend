99d05e63918cbf659ac7e9ff2750209ec4e63bc4
schoollovewater <quetruong03@gmail.com>
2026-05-31 15:31:22 +0700

    integrate frontend to public folder

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const app = express();
 app.use(cors()); // Cấp quyền cho file HTML kết nối vào server
 
+// ==========================================
+// 🚀 BƯỚC 3: THÊM DÒNG NÀY VÀO ĐÂY NÈ BẠN
+app.use(express.static('public'));
+// ==========================================
+
 const server = http.createServer(app);
 
 // Cấu hình Socket.io nhận mọi kết nối từ các nguồn (bao gồm cả local và online)
@@ -26,7 +31,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Cấu hình cổng chạy Server (Tương thích hoàn toàn với nền tảng Render)
+// Cấu hình cổng chạy Server (Tương thích hoàn toàn với nền tảng Vercel/Render)
 const PORT = process.env.PORT || 5000; 
 server.listen(PORT, () => {
     console.log(`Backend Ma Sói đang chạy online tại cổng: ${PORT}`);
